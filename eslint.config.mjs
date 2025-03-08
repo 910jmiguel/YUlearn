5abcd48403306ff54cdcb4ca2fbd34c9636e4162
Ahmed <ahmedjader837@gmail.com>
2025-03-08 15:49:19 -0500

    disable unused vars and img element warnings in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // Disable unused vars rule
+      "next/next/no-img-element": "off", // Optional: Disable img warning
+    },
+  },
 ];
 
 export default eslintConfig;
