19dc3904057c08397bb5b02710f167294e13edf2
Ahmed <ahmedjader837@gmail.com>
2025-03-09 12:17:08 -0400

    update About page footer styling and add new SVG logo

--- app/pages/about/page.tsx
+++ app/pages/about/page.tsx
@@ -14,8 +14,8 @@ const Page = () => {
       </main>
 
       {/* Footer */}
-      <footer className="p-4 w-full text-start bg-red-600">
-        <p className="text-sm text-gray-600">
+      <footer className="p-4 w-full text-end bg-red-600">
+        <p className="text-sm text-white">
           © {new Date().getFullYear()} YuLearn. All rights reserved.
         </p>
       </footer>
