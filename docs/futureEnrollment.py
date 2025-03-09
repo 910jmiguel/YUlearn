bec34fb6db345b354699f073307443062b4864f9
Derek Song <dereksong28@gmail.com>
2025-03-08 22:14:34 -0500

    linear-ish regression?

--- docs/futureEnrollment.py
+++ docs/futureEnrollment.py
@@ -30,7 +30,6 @@ for target in target_columns:
     
     formula = f"{target}: y = {slope:.2f} * Year + {intercept:.2f}"
     formulas.append(formula)
-    print(formula)
 
     
     yorkPredictionYear = np.array([2025, 2026, 2027, 2028, 2029, 2030]).reshape(-1, 1)
@@ -59,3 +58,60 @@ for target in target_columns:
 
 finalData["Year"] = finalData["Year"].apply(lambda x: f"{x}-{str(int(x) + 1)[2:]}" if x.isdigit() else x)
 finalData.to_csv("docs/YorkLassonde Data Predictions.csv", index=False)
+
+
+formula_df = pd.DataFrame({
+    "Major": target_columns,
+    "Formula": formulas
+})
+
+formula_df.to_csv("docs/YorkLassonde_Formulas.csv", index=False)
+
+fig = go.Figure()
+
+n = 0
+plotlyColour= ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd']
+print(predictions)
+for target, pred in zip(target_columns, predictions):
+    fig.add_trace(go.Scatter(
+        x=yorkDataCopy['Year'].astype(str),
+        y=yorkDataCopy[target],
+        mode='lines',
+        name=f"{target} Actual",
+        line=dict(color=plotlyColour[n], width=2)
+    ))
+
+    last_actual_year = yorkDataCopy['Year'].astype(str).iloc[-1]
+    first_predicted_year = pred['Year'].iloc[0]
+    last_actual_value = yorkDataCopy[target].iloc[-1]
+    first_predicted_value = pred[target].iloc[0]
+
+    fig.add_trace(go.Scatter(
+        x=[last_actual_year, first_predicted_year],
+        y=[last_actual_value, first_predicted_value],
+        mode='lines',
+        name=f"{target} Predicted",
+        line=dict(color=plotlyColour[n], width=2, dash='dot')  # Dashed line
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=pred['Year'],
+        y=pred[target],
+        mode='lines',
+        name=f"{target} Predicted",
+        line=dict(color=plotlyColour[n], width=2, dash='dot')
+    ))
+    n += 1
+
+    fig.update_layout(
+        title="Enrollment predictions using a linear regression model",
+        xaxis_title="Year",
+        yaxis_title="Enrollment",
+        showlegend=True,
+        font=dict(
+            family="Avenir, sans-serif", 
+        )
+    )
+
+fig.show()
+fig.write_image("enrollmentLassonde.pdf", scale=2)
